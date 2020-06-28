e6830e0233d3feb7693481a64908e171ce1cc765
sunlu <trista090923@gmail.com>
2020-06-28 19:19:43 +0900

    添加项目文件。

---
+++ enpra05_a/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace enpra05_a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Double NUM1 = Convert.ToDouble(NUM_1_TEXTBOX.Text.ToString());
+            Double NUM2 = Convert.ToDouble(NUM_2_TEXTBOX.Text.ToString());
+            string ch = cal_ComboBox.Text;
+            string result = "";
+            switch (ch)
+            {
+                case "+":
+                    result = (NUM1 + NUM2).ToString();
+                    OUTPUT_TEXTBOX.Text = result;
+                    break;
+                case "-":
+                    result = (NUM1 - NUM2).ToString();
+                    OUTPUT_TEXTBOX.Text = result;
+                    break;
+                case "*":
+                    result = (NUM1 * NUM2).ToString();
+                    OUTPUT_TEXTBOX.Text = result;
+                    break;
+                case "/":
+                    result = (NUM1 / NUM2).ToString();
+                    OUTPUT_TEXTBOX.Text = result;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
